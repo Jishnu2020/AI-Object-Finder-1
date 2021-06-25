f8aac045cf5154d84d110a860ecbf1dc06c4c41a
Jishnu2020 <73780665+Jishnu2020@users.noreply.github.com>
2021-06-25 16:50:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function setup() {
+canvas = createCanvas(380, 380);
+canvas.center();
+video = createCapture(VIDEO);
+video.hide();
+video.size(380, 380);
+objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function draw() {
+image(video, 0, 0, 380, 380);
+}
+function modelLoaded() {
+console.log("Model Loaded!");
+}
